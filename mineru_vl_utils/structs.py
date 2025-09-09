d87debf06b852af3b57edd5f3476c16515980279
Jin Zhen Jiang <jinzhenjiang@pjlab.org.cn>
2025-09-09 16:55:10 +0800

    feat: delete unused block types

--- mineru_vl_utils/structs.py
+++ mineru_vl_utils/structs.py
@@ -2,94 +2,57 @@ from typing import Literal
 
 
 class BlockType:
-    SUPER = "super"  # 超级块(整个文档)
-
     TEXT = "text"  # 文本
     TITLE = "title"  # 段落标题
     TABLE = "table"  # 表格
     IMAGE = "image"  # 图像
-    CHART = "chart"  # 图表(柱状图,饼图等)
     CODE = "code"  # 代码
     ALGORITHM = "algorithm"  # 算法/伪代码
-    ABSTRACT = "abstract"  # 摘要
-    TOC = "toc"  # 目录
-    SEAL = "seal"  # 印章
     HEADER = "header"  # 页眉
     FOOTER = "footer"  # 页脚
-    PAGE_TITLE = "page_title"  # 文档标题
     PAGE_NUMBER = "page_number"  # 页码
     PAGE_FOOTNOTE = "page_footnote"  # 脚注
     ASIDE_TEXT = "aside_text"  # 侧栏文本(装订线等)
     EQUATION = "equation"  # 公式(独立或行内)
     EQUATION_BLOCK = "equation_block"  # 公式块(多行公式)
-    EQUATION_NUMBER = "equation_number"  # 公式编号
     REF_TEXT = "ref_text"  # 参考文献(一条)
-    REF_BLOCK = "ref_block"  # 参考文献(大块)
     LIST = "list"  # 列表块(无序/有序列表)
-    LIST_ITEM = "list_item"  # 列表项(无序/有序列表)
-    MOLECULAR = "molecular"  # 化学分子结构
-    REACTION = "reaction"  # 化学反应
     PHONETIC = "phonetic"  # 注音符号
 
     # captions
-    COMMON_CAPTION = "common_caption"  # 通用标题(table/image/chart)
     TABLE_CAPTION = "table_caption"  # 表格标题
     IMAGE_CAPTION = "image_caption"  # 图像标题
-    CHART_CAPTION = "chart_caption"  # 图表标题
     CODE_CAPTION = "code_caption"  # 代码标题
-    COMMON_FOOTNOTE = "common_footnote"  # 通用脚注
     TABLE_FOOTNOTE = "table_footnote"  # 表格脚注
     IMAGE_FOOTNOTE = "image_footnote"  # 图像脚注
-    CHART_FOOTNOTE = "chart_footnote"  # 图表脚注
 
-    QRCODE = "qrcode"  # 二维码
-    BARCODE = "barcode"  # 条形码
-    ABANDON = "abandon"  # 废弃块
     UNKNOWN = "unknown"  # 未知块
 
 
 BLOCK_TYPES = set(
     [
-        BlockType.SUPER,
         BlockType.TEXT,
         BlockType.TITLE,
         BlockType.TABLE,
         BlockType.IMAGE,
-        BlockType.CHART,
         BlockType.CODE,
-        BlockType.ABSTRACT,
-        BlockType.TOC,
-        BlockType.SEAL,
         BlockType.HEADER,
         BlockType.FOOTER,
-        BlockType.PAGE_TITLE,
         BlockType.PAGE_NUMBER,
         BlockType.PAGE_FOOTNOTE,
         BlockType.ASIDE_TEXT,
         BlockType.EQUATION,
         BlockType.EQUATION_BLOCK,
-        BlockType.EQUATION_NUMBER,
         BlockType.REF_TEXT,
-        BlockType.REF_BLOCK,
-        BlockType.COMMON_CAPTION,
         BlockType.TABLE_CAPTION,
         BlockType.IMAGE_CAPTION,
-        BlockType.CHART_CAPTION,
-        BlockType.COMMON_FOOTNOTE,
         BlockType.TABLE_FOOTNOTE,
         BlockType.IMAGE_FOOTNOTE,
-        BlockType.CHART_FOOTNOTE,
-        BlockType.QRCODE,
-        BlockType.BARCODE,
-        BlockType.ABANDON,
         BlockType.ALGORITHM,
         BlockType.CODE_CAPTION,
         BlockType.LIST,
-        BlockType.LIST_ITEM,
-        BlockType.MOLECULAR,
-        BlockType.REACTION,
-        BlockType.UNKNOWN,
         BlockType.PHONETIC,
+        BlockType.UNKNOWN,
     ]
 )
 
