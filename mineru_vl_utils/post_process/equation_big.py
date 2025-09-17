6c383d86629f5d65e5251efa245ff44c7f71d466
JulioZhao97 <zhaozhiyuan@buaa.edu.cn>
2025-09-17 19:48:54 +0800

    fix \big{\rangle} bug in gradio

--- mineru_vl_utils/post_process/equation_big.py
+++ mineru_vl_utils/post_process/equation_big.py
@@ -243,9 +243,103 @@ def try_fix_equation_big(latex: str, debug: bool = False) -> str:
     latex = re.sub(r"\\biggl \{\\\|\}", r"\\biggl\|", latex)
     latex = re.sub(r"\\Biggl \{\\\|\}", r"\\Biggl\|", latex)
     
+    
+    # ------------------ \big{\rangle} -> \big\rangle ------------------ #
+    
+    # \big
+    latex = re.sub(r"\\big\{\\rangle\}", r"\\big\\rangle ", latex)
+    latex = re.sub(r"\\big\{\\langle\}", r"\\big\\langle ", latex)
+    latex = re.sub(r"\\big \{\\rangle\}", r"\\big\\rangle ", latex)
+    latex = re.sub(r"\\big \{\\langle\}", r"\\big\\langle ", latex)
+    
+    # \bigr
+    latex = re.sub(r"\\bigr\{\\rangle\}", r"\\bigr\\rangle ", latex)
+    latex = re.sub(r"\\bigr\{\\langle\}", r"\\bigr\\langle ", latex)
+    latex = re.sub(r"\\bigr \{\\rangle\}", r"\\bigr\\rangle ", latex)
+    latex = re.sub(r"\\bigr \{\\langle\}", r"\\bigr\\langle ", latex)
+    
+    # \bigm
+    latex = re.sub(r"\\bigm\{\\rangle\}", r"\\bigm\\rangle ", latex)
+    latex = re.sub(r"\\bigm\{\\langle\}", r"\\bigm\\langle ", latex)
+    latex = re.sub(r"\\bigm \{\\rangle\}", r"\\bigm\\rangle ", latex)
+    latex = re.sub(r"\\bigm \{\\langle\}", r"\\bigm\\langle ", latex)
+    
+    # \bigl
+    latex = re.sub(r"\\bigl\{\\rangle\}", r"\\bigl\\rangle ", latex)
+    latex = re.sub(r"\\bigl\{\\langle\}", r"\\bigl\\langle ", latex)
+    latex = re.sub(r"\\bigl \{\\rangle\}", r"\\bigl\\rangle ", latex)
+    latex = re.sub(r"\\bigl \{\\langle\}", r"\\bigl\\langle ", latex)
+    
+    # \bigg
+    latex = re.sub(r"\\bigg\{\\rangle\}", r"\\bigg\\rangle ", latex)
+    latex = re.sub(r"\\bigg\{\\langle\}", r"\\bigg\\langle ", latex)
+    latex = re.sub(r"\\bigg \{\\rangle\}", r"\\bigg\\rangle ", latex)
+    latex = re.sub(r"\\bigg \{\\langle\}", r"\\bigg\\langle ", latex)
+    
+    # \biggr
+    latex = re.sub(r"\\biggr\{\\rangle\}", r"\\biggr\\rangle ", latex)
+    latex = re.sub(r"\\biggr\{\\langle\}", r"\\biggr\\langle ", latex)
+    latex = re.sub(r"\\biggr \{\\rangle\}", r"\\biggr\\rangle ", latex)
+    latex = re.sub(r"\\biggr \{\\langle\}", r"\\biggr\\langle ", latex)
+    
+    # \biggm
+    latex = re.sub(r"\\biggm\{\\rangle\}", r"\\biggm\\rangle ", latex)
+    latex = re.sub(r"\\biggm\{\\langle\}", r"\\biggm\\langle ", latex)
+    latex = re.sub(r"\\biggm \{\\rangle\}", r"\\biggm\\rangle ", latex)
+    latex = re.sub(r"\\biggm \{\\langle\}", r"\\biggm\\langle ", latex)
+    
+    # \biggl
+    latex = re.sub(r"\\biggl\{\\rangle\}", r"\\biggl\\rangle ", latex)
+    latex = re.sub(r"\\biggl\{\\langle\}", r"\\biggl\\langle ", latex)
+    latex = re.sub(r"\\biggl \{\\rangle\}", r"\\biggl\\rangle ", latex)
+    latex = re.sub(r"\\biggl \{\\langle\}", r"\\biggl\\langle ", latex)
+    
+    # \Big
+    latex = re.sub(r"\\Big\{\\rangle\}", r"\\Big\\rangle ", latex)
+    latex = re.sub(r"\\Big\{\\langle\}", r"\\Big\\langle ", latex)
+    latex = re.sub(r"\\Big \{\\rangle\}", r"\\Big\\rangle ", latex)
+    latex = re.sub(r"\\Big \{\\langle\}", r"\\Big\\langle ", latex)
+    
+    # \Bigr
+    latex = re.sub(r"\\Bigr\{\\rangle\}", r"\\Bigr\\rangle ", latex)
+    latex = re.sub(r"\\Bigr\{\\langle\}", r"\\Bigr\\langle ", latex)
+    latex = re.sub(r"\\Bigr \{\\rangle\}", r"\\Bigr\\rangle ", latex)
+    latex = re.sub(r"\\Bigr \{\\langle\}", r"\\Bigr\\langle ", latex)
+    
+    # \Bigm
+    latex = re.sub(r"\\Bigm\{\\rangle\}", r"\\Bigm\\rangle ", latex)
+    latex = re.sub(r"\\Bigm\{\\langle\}", r"\\Bigm\\langle ", latex)
+    latex = re.sub(r"\\Bigm \{\\rangle\}", r"\\Bigm\\rangle ", latex)
+    latex = re.sub(r"\\Bigm \{\\langle\}", r"\\Bigm\\langle ", latex)
+    
+    # \Bigl
+    latex = re.sub(r"\\Bigl\{\\rangle\}", r"\\Bigl\\rangle ", latex)
+    latex = re.sub(r"\\Bigl\{\\langle\}", r"\\Bigl\\langle ", latex)
+    latex = re.sub(r"\\Bigl \{\\rangle\}", r"\\Bigl\\rangle ", latex)
+    latex = re.sub(r"\\Bigl \{\\langle\}", r"\\Bigl\\langle ", latex)
+    
+    # \Bigg
+    latex = re.sub(r"\\Bigg\{\\rangle\}", r"\\Bigg\\rangle ", latex)
+    latex = re.sub(r"\\Bigg\{\\langle\}", r"\\Bigg\\langle ", latex)
+    latex = re.sub(r"\\Bigg \{\\rangle\}", r"\\Bigg\\rangle ", latex)
+    latex = re.sub(r"\\Bigg \{\\langle\}", r"\\Bigg\\langle ", latex)
+    
+    # \Biggr
+    latex = re.sub(r"\\Biggr\{\\rangle\}", r"\\Biggr\\rangle ", latex)
+    latex = re.sub(r"\\Biggr\{\\langle\}", r"\\Biggr\\langle ", latex)
+    latex = re.sub(r"\\Biggr \{\\rangle\}", r"\\Biggr\\rangle ", latex)
+    latex = re.sub(r"\\Biggr \{\\langle\}", r"\\Biggr\\langle ", latex)
+    
+    # \Biggl
+    latex = re.sub(r"\\Biggl\{\\rangle\}", r"\\Biggl\\rangle ", latex)
+    latex = re.sub(r"\\Biggl\{\\langle\}", r"\\Biggl\\langle ", latex)
+    latex = re.sub(r"\\Biggl \{\\rangle\}", r"\\Biggl\\rangle ", latex)
+    latex = re.sub(r"\\Biggl \{\\langle\}", r"\\Biggl\\langle ", latex)
+    
+    
     return latex
 
 
 if __name__ == "__main__":
-    latex = r"\begin{array}{l} \mathcal {P} _ {z _ {1}; \iota u} \omega_ {| I | + 3} ^ {(0)} \left(z _ {2}, z _ {1}, u, I\right) \tag {32} \\ = \operatorname *{Res}_{\tilde{q} = \iota u}\frac{dz_{1}}{z_{1} - \tilde{q}}\Bigl {\{}2\sum_{i}\operatorname *{Res}_{q = \beta_{i}}K_{i}(z_{2},q)\sum_{\substack{I^{\prime}\sqcup I^{\prime \prime} = I\\ I^{\prime \prime}\neq \emptyset}}\omega_{|I^{\prime}| + 3}^{(0)}(q,\tilde{q},u,I^{\prime})\omega_{|I^{\prime \prime}| + 1}^{(0)}(\sigma_{i}(q),I^{\prime \prime})\quad (\dagger) \\ \end{array}"
+    latex = r"\begin{array}{l} \widehat {J} (\zeta ; u) \\ = \frac{1}{2}\mathbb{E}\left[\int_{0}^{T}\Bigg{\langle}\left( \begin{array}{ccc}0 & S_{1} + \Phi C & S_{2} + \Phi B\\ (S_{1} + \Phi C)^{\top} & R_{11} + \Phi & R_{12}\\ (S_{2} + \Phi B)^{\top} & R_{12}^{\top} & R_{22} \end{array} \right)\left( \begin{array}{c}Y - \mathbb{E}[Y]\\ Z - \mathbb{E}[Z]\\ u - \mathbb{E}[u] \end{array} \right),\left( \begin{array}{c}Y - \mathbb{E}[Y]\\ Z - \mathbb{E}[Z]\\ u - \mathbb{E}[u] \end{array} \right)\Bigg{\rangle}dt\right. \\ + \int_ {0} ^ {T} \left\langle \left( \begin{array}{c c c} 0 & \widetilde {S} _ {1} + \widetilde {\Phi} \widetilde {C} & \widetilde {S} _ {2} + \widetilde {\Phi} \widetilde {B} \\ (\widetilde {S} _ {1} + \widetilde {\Phi} \widetilde {C}) ^ {\top} & \widetilde {R} _ {1 1} + \Phi & \widetilde {R} _ {1 2} \\ (\widetilde {S} _ {2} + \widetilde {\Phi} \widetilde {B}) ^ {\top} & \widetilde {R} _ {1 2} ^ {\top} & \widetilde {R} _ {2 2} \end{array} \right) \left( \begin{array}{c} \mathbb {E} [ Y ] \\ \mathbb {E} [ Z ] \\ \mathbb {E} [ u ] \end{array} \right), \left( \begin{array}{c} \mathbb {E} [ Y ] \\ \mathbb {E} [ Z ] \\ \mathbb {E} [ u ] \end{array} \right) \right\rangle d t \\ \left. + 2 \int_ {0} ^ {T} \left\langle \left( \begin{array}{c} \widetilde {q} \\ \rho_ {1} \\ \rho_ {2} \end{array} \right), \left( \begin{array}{c} Y \\ Z \\ u \end{array} \right) \right\rangle d t + 2 \langle g, Y (0) \rangle \right]. \\ \end{array}"
     print(try_fix_equation_big(latex))
