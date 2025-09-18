07032a404641e9d66772ec7e08fde9dfa396365a
Jin Zhen Jiang <jinzhenjiang@pjlab.org.cn>
2025-09-18 08:52:30 +0800

    feat: change version

--- mineru_vl_utils/__init__.py
+++ mineru_vl_utils/__init__.py
@@ -1,10 +1,11 @@
-from .mineru_client import MinerUClient
+from .mineru_client import MinerUClient, MinerUSamplingParams
 
-__version__ = "0.1.5"
-__version_info__ = (0, 1, 5)
+__version__ = "0.1.6"
+__version_info__ = (0, 1, 6)
 
 __all__ = [
     "MinerUClient",
+    "MinerUSamplingParams",
     "__version__",
     "__version_info__",
 ]
